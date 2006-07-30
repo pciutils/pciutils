1c5cc9b5b323c84179cad9d90e86600d3c0cdfb6
Martin Mares <mj@ucw.cz>
2006-07-30 12:41:38 +0200

    Polishing the Windows port.

--- lib/i386-io-windows.h
+++ lib/i386-io-windows.h
@@ -9,11 +9,11 @@
 #include <io.h>
 #include <conio.h>
 #include <windows.h>
-  
+
 #define outb(x,y) _outp(y,x)
 #define outw(x,y) _outpw(y,x)
 #define outl(x,y) _outpd(y,x)
-  
+
 #define inb(x) _inp(x)
 #define inw(x) _inpw(x)
 #define inl(x) _inpd(x)
@@ -26,7 +26,7 @@ intel_setup_io(void)
   if (intel_iopl_set < 0)
     {
       typedef int (*MYPROC)(void);
-      MYPROC InitializeWinIo;  
+      MYPROC InitializeWinIo;
       HMODULE lib;
 
       intel_iopl_set = 0;
@@ -37,12 +37,13 @@ intel_setup_io(void)
 	  fprintf(stderr, "libpci: Couldn't load WinIo.dll.\n");
 	  return 0;
 	}
+      /* XXX: Is this really needed? --mj */
       GetProcAddress(lib, "InitializeWinIo");
-  
+
       InitializeWinIo = (MYPROC) GetProcAddress(lib, "InitializeWinIo");
       if (!InitializeWinIo)
 	{
-	  fprintf(stderr, "libpci: Couldn't find InitialieWinIo function.\n");
+	  fprintf(stderr, "libpci: Couldn't find InitializeWinIo function.\n");
 	  return 0;
 	}
 
@@ -52,7 +53,7 @@ intel_setup_io(void)
 	  return 0;
 	}
 
-      intel_iopl_set=1;
+      intel_iopl_set = 1;
     }
   return intel_iopl_set;
 }
