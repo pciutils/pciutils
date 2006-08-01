4254d153e43d50bd8300d8be06b46a5d9e3982b0
Martin Mares <mj@ucw.cz>
2006-08-01 12:19:09 +0200

    Fixed a silly typo.

--- lib/i386-io-hurd.h
+++ lib/i386-io-hurd.h
@@ -25,15 +25,15 @@ intel_setup_io(struct pci_access *a)
   mach_port_t device;
 
   if ((errno = get_privileged_ports(NULL, &device)))
-    a->warn("i386-io-hurd: Can't get_privileged_ports(): %m");
+    a->warning("i386-io-hurd: Can't get_privileged_ports(): %m");
 
   if (!errno && (errno = device_open(device, D_READ | D_WRITE, "io", &io_port)))
-    a->warn("i386-io-hurd: Can't device_open(): %m");
+    a->warning("i386-io-hurd: Can't device_open(): %m");
 
   mach_port_deallocate(mach_task_self(), device);
 
   if (!errno && (errno = i386_io_port_add(mach_thread_self(), io_port)))
-    a->warn("i386-io-hurd: Can't i386_io_port_add(): %m");
+    a->warning("i386-io-hurd: Can't i386_io_port_add(): %m");
 
   return errno ? 0 : 1;
 }
@@ -42,7 +42,7 @@ static inline int
 intel_cleanup_io(struct pci_access *a)
 {
   if ((errno = i386_io_port_remove(mach_thread_self(), io_port)))
-    a->warn("i386-io-hurd: Can't i386_io_port_remove(): %m");
+    a->warning("i386-io-hurd: Can't i386_io_port_remove(): %m");
 
   mach_port_deallocate(mach_task_self(), io_port);
 
