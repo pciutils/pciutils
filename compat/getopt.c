4c2bdb019375b5b2167959a167710791225b6670
Martin Mares <mj@ucw.cz>
2010-12-04 18:33:09 +0100

    compat/getopt.c: Fixed a typo in declaration of getopt_long()

--- compat/getopt.c
+++ compat/getopt.c
@@ -619,7 +619,7 @@ int getopt(int argc, char *const *argv, const char *optstring)
 	return _getopt_internal(argc, argv, optstring, (const struct option *) 0, (int *) 0, 0);
 }
 
-int getopt_long(int argc, char *const *argv, const char *options, const struct option long_options, int *opt_index)
+int getopt_long(int argc, char *const *argv, const char *options, const struct option *long_options, int *opt_index)
 {
 	return _getopt_internal(argc, argv, options, long_options, opt_index, 0);
 }
