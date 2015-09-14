5bdd27f66a98943e9e88cd75c7b637f10b420484
Martin Mares <mj@ucw.cz>
2015-09-14 16:48:32 +0200

    Whitespace fixes

--- lib/pci.h
+++ lib/pci.h
@@ -39,7 +39,7 @@ enum pci_access_type {
   PCI_ACCESS_AIX_DEVICE,		/* /dev/pci0, /dev/bus0, etc. */
   PCI_ACCESS_NBSD_LIBPCI,		/* NetBSD libpci */
   PCI_ACCESS_OBSD_DEVICE,		/* OpenBSD /dev/pci */
-  PCI_ACCESS_DUMP,			    /* Dump file */
+  PCI_ACCESS_DUMP,			/* Dump file */
   PCI_ACCESS_DARWIN,			/* Darwin */
   PCI_ACCESS_MAX
 };
