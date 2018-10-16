03db2fa5689868b2e4cdfebcdcbb8b31f53b5904
Daniel Schaefer <git@danielschaefer.me>
2018-10-16 20:45:25 +0200

    Fix regression of refactoring in d7d9e30534eb551
    The index has to be increased only when the capability id matches
    because we want to keep track of the number of capabilities with that
    ID.

--- lib/caps.c
+++ lib/caps.c
@@ -113,8 +113,8 @@ pci_find_cap(struct pci_dev *d, unsigned int id, unsigned int type)
  * To select one capability if there are more than one with the same id, you
  * can provide a pointer to an unsigned int that contains the index which you
  * want as cap_number. If you don't care and are fine with the first one you
- * can supply NULL. The cap_number will be replaced by the acutal number
- * of capablities with that id.
+ * can supply NULL. The cap_number will be replaced by the actual number of
+ * capablities with that id.
  */
 struct pci_cap *
 pci_find_cap_nr(struct pci_dev *d, unsigned int id, unsigned int type,
@@ -130,11 +130,13 @@ pci_find_cap_nr(struct pci_dev *d, unsigned int id, unsigned int type,
   for (c=d->first_cap; c; c=c->next)
     {
       if (c->type == type && c->id == id)
-	{
-	  if (target == index)
-	    found = c;
-	}
-      index++;
+        {
+          if (target == index)
+            {
+              found = c;
+            }
+          index++;
+        }
     }
 
   if (cap_number)
